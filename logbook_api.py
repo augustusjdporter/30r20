dd6142b680c53bc3a090449f04f2613adc8e390d
Augustus Porter <augustusporter@Augustuss-MBP.cust.communityfibre.co.uk>
2023-11-03 20:20:50 +0000

    sorting out tab

--- logbook_api.py
+++ logbook_api.py
@@ -15,30 +15,30 @@ client = WebApplicationClient(client_id)
 authorization_url = 'https://log.concept2.com/oauth/authorize'
 
 def get_logbook_data():
-  url = client.prepare_request_uri(
+    url = client.prepare_request_uri(
     authorization_url,
     redirect_uri = 'http://localhost',
     scope = ['results:read'],
-  )
+    )
 
-  print(url)
+    print(url)
 
-  browser = Chrome(window_geometry=(100, 22, 400, 690))
-  # Pass Authentication URL
-  browser.open_new_window(url)
-  # Initialise Wait
-  wait = Wait(browser)
-  # Wait till query "code" is present in the URL.
-  wait.until_present_query('code')
-  # Fetch the url
-  response_url = urlparse(browser.get_current_url())
-  code = parse_qs(response_url.query).get('code')[0]
-  print("\nCode: %s\n" % code)
+    browser = Chrome(window_geometry=(100, 22, 400, 690))
+    # Pass Authentication URL
+    browser.open_new_window(url)
+    # Initialise Wait
+    wait = Wait(browser)
+    # Wait till query "code" is present in the URL.
+    wait.until_present_query('code')
+    # Fetch the url
+    response_url = urlparse(browser.get_current_url())
+    code = parse_qs(response_url.query).get('code')[0]
+    print("\nCode: %s\n" % code)
 
-  client_secret = get_secrets()["logbook_client_secret"]
+    client_secret = get_secrets()["logbook_client_secret"]
 
-  headers = {"Content-Type": "application/x-www-form-urlencoded"}
-  ret = requests.post(url="https://log.concept2.com/oauth/access_token",
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    ret = requests.post(url="https://log.concept2.com/oauth/access_token",
                       data={"client_id": client_id,
                             "client_secret": client_secret,
                             "code": code,
@@ -46,16 +46,16 @@ def get_logbook_data():
                             "redirect_uri": "http://localhost",
                             "scope": "results:read"},
                       headers=headers)
-  access_token = ret.json()['access_token']
-  refresh_token = ret.json()['refresh_token']
+    access_token = ret.json()['access_token']
+    refresh_token = ret.json()['refresh_token']
 
-  header = {'Authorization': 'Bearer ' + access_token}
-  print(access_token)
-  ret = requests.get(url="https://log.concept2.com/api/users/me/results?from=2023-08-21&to=2023-11-03&type=rower",
+    header = {'Authorization': 'Bearer ' + access_token}
+    print(access_token)
+    ret = requests.get(url="https://log.concept2.com/api/users/me/results?from=2023-08-21&to=2023-11-03&type=rower",
                     headers=header).json()
-  print(ret)
-  ret_df = pd.DataFrame(ret["data"])
-  ret_df = ret_df[["date", "distance", "time", "stroke_rate"]]
-  thirty = ret_df[(ret_df["time"]==18000) & (ret_df["distance"] >= 8000)]
-  print(thirty)
-  return thirty
+    print(ret)
+    ret_df = pd.DataFrame(ret["data"])
+    ret_df = ret_df[["date", "distance", "time", "stroke_rate"]]
+    thirty = ret_df[(ret_df["time"]==18000) & (ret_df["distance"] >= 8000)]
+    print(thirty)
+    return thirty
